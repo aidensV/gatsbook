29d51e29c91b12c3b70624277e6bc07a5426ddbd
aidensV <nameblacllis@gmail.com>
2021-08-04 21:05:15 +0700

    add new image

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,16 +8,17 @@ import "../styles/test.css"
 const IndexPage = () => {
   return (
     <main>
+        <p>Tap to open</p>
       <div class="book">
-    <div class="back"></div>
-    <div class="page6"></div>
-    <div class="page5"></div>
-    <div class="page4"></div>
-    <div class="page3"></div>
-    <div class="page2"></div>
-    <div class="page1"></div>
-    <div class="front"></div>
-</div>
+        <div class="back"></div>
+        <div class="page6"></div>
+        <div class="page5"></div>
+        <div class="page4"></div>
+        <div class="page3"></div>
+        <div class="page2"></div>
+        <div class="page1"></div>
+        <div class="front"></div>
+    </div>
     </main>
   )
 }
